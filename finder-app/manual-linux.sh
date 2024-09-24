ceab02a19d24c45b30f52560a21571d08f0c2ae6
rishap <rishapsingh704@gmail.com>
2024-09-24 23:25:07 +0530

    Corrected PAth for dependency try1

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -97,8 +97,8 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # TODO: Add library dependencies to rootfs
 echo "Add dependency" 
 cd "$OUTDIR"
-cp ${DEPENDENCY}/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp ${DEPENDENCY}/libc.so.6 ${DEPENDENCY}/libm.so.6 ${DEPENDENCY}/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /home/rishap/rishap/Coursera/LinuxSystemProgramming/assignment-1-rishap777/dependency/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp /home/rishap/rishap/Coursera/LinuxSystemProgramming/assignment-1-rishap777/dependency/libc.so.6 /home/rishap/rishap/Coursera/LinuxSystemProgramming/assignment-1-rishap777/dependency/libm.so.6 /home/rishap/rishap/Coursera/LinuxSystemProgramming/assignment-1-rishap777/dependency/libresolv.so.2 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
 cd "$OUTDIR/rootfs"
